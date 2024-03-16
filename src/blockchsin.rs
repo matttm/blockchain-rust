bae19ad6f9a39312a9e26f19af9f18fae38ab0a7
matttm <matttmaloney@gmail.com>
2024-03-16 10:18:15 -0400

    writing block genesis

--- src/blockchsin.rs
+++ src/blockchsin.rs
@@ -1,6 +1,20 @@
 
 pub struct State {
     pub blocks: Vec,
+    fn new() -> Self {
+        Self { blocks: vec![] }
+    }
+    fn create_genesis(&mut self) {
+        let genesis_block = {
+            id: 0,
+            timestamp: Utc::now().timestamp(),
+            previous_hash: String::from("genesis"),
+            data: String::from("genesis!"),
+            nonce: 2836,
+            hash: "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43".to_string(),
+        }
+        self.blocks.push(block)
+    }
 }
 
 pub struct Block {
