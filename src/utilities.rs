dda5dbc9c58e3bdb69fcdc4283dc00e761547267
matttm <matttmaloney@gmail.com>
2024-03-16 17:29:49 -0400

    writing converter

---
+++ src/utilities.rs
@@ -0,0 +1,9 @@
+const DIFFICULTY_PREFIX = "00";
+
+fn hash_to_binary(hash: &[u8]) -> String {
+    let mut result = String::default();
+    for c in hash {
+        result.push_str(&format!("{:b}", c));
+    }
+    result
+}
