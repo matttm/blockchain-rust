08d452a5d501e52c2538843131ce03a31d1cf580
matttm <matttmaloney@gmail.com>
2024-04-21 15:52:59 -0400

    trying to update

--- src/p2p.rs
+++ src/p2p.rs
@@ -4,10 +4,12 @@ use crate::State;
 use libp2p::{
     floodsub::{Floodsub, FloodsubEvent, Topic},
     identity,
-    mdns::{Mdns, MdnsEvent},
-    swarm::{NetworkBehaviourEventProcess, Swarm},
-    NetworkBehaviour, PeerId,
+    mdns::Event as MdnsEvent,
+    swarm::NetworkBehaviour,
+    swarm::Swarm,
+    PeerId,
 };
+
 use log::{error, info};
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
@@ -37,6 +39,7 @@ pub enum EventType {
 }
 
 #[derive(NetworkBehaviour)]
+#[behaviour(to_swarm = "EventType")]
 pub struct StateBehavior {
     pub floodsub: Floodsub,
     pub mdns: Mdns,
@@ -64,56 +67,6 @@ impl StateBehavior {
         behavior.floodsub.subscribe(CHAIN_TOPIC.clone());
         behavior.floodsub.subscribe(BLOCK_TOPIC.clone());
         behavior
-    }
-}
-
-impl NetworkBehaviourEventProcess<MdnsEvent> for StateBehavior {
-    fn inject_event(&mut self, event: MdnsEvent) {
-        match event {
-            MdnsEvent::Discovered(discovered_list) => {
-                for (peer, _addr) in discovered_list {
-                    println!("Peer discovered");
-                    self.floodsub.add_node_to_partial_view(peer);
-                }
-            }
-            MdnsEvent::Expired(expired_list) => {
-                for (peer, _addr) in expired_list {
-                    if !self.mdns.has_node(&peer) {
-                        println!("A peer disconnected");
-                        self.floodsub.remove_node_from_partial_view(&peer)
-                    }
-                }
-            }
-        }
-    }
-}
-
-impl NetworkBehaviourEventProcess<FloodsubEvent> for StateBehavior {
-    fn inject_event(&mut self, event: FloodsubEvent) {
-        if let FloodsubEvent::Message(msg) = event {
-            if let Ok(resp) = serde_json::from_slice::<ChainResponse>(&msg.data) {
-                if resp.receiver == PEER_ID.to_string() {
-                    info!("Response from {}:", msg.source);
-                    resp.blocks.iter().for_each(|r| info!("{:?}", r));
-
-                    self.state.blocks = State::choose_chain(self.state.blocks.clone(), resp.blocks);
-                }
-            } else if let Ok(resp) = serde_json::from_slice::<LocalChainRequest>(&msg.data) {
-                info!("sending local chain to {}", msg.source.to_string());
-                let peer_id = resp.from_peer_id;
-                if PEER_ID.to_string() == peer_id {
-                    if let Err(e) = self.response_sender.send(ChainResponse {
-                        blocks: self.state.blocks.clone(),
-                        receiver: msg.source.to_string(),
-                    }) {
-                        error!("error sending response via channel, {}", e);
-                    }
-                }
-            } else if let Ok(block) = serde_json::from_slice(&msg.data) {
-                info!("received new block from {}", msg.source.to_string());
-                self.state.add_block(block);
-            }
-        }
     }
 }
 
