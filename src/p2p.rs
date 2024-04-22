a0169d4e2d4e4d7aab8b4e571b4793a2a86ed4a0
matttm <matttmaloney@gmail.com>
2024-04-21 20:51:22 -0400

    updating libp2p

--- src/p2p.rs
+++ src/p2p.rs
@@ -4,7 +4,6 @@ use crate::State;
 use libp2p::{
     floodsub::{Floodsub, FloodsubEvent, Topic},
     identity,
-    mdns::Event as MdnsEvent,
     swarm::NetworkBehaviour,
     swarm::Swarm,
     PeerId,
@@ -39,10 +38,10 @@ pub enum EventType {
 }
 
 #[derive(NetworkBehaviour)]
+//#[behaviour(event_process = true)]
 #[behaviour(to_swarm = "EventType")]
 pub struct StateBehavior {
     pub floodsub: Floodsub,
-    pub mdns: Mdns,
     #[behaviour(ignore)]
     pub response_sender: mpsc::UnboundedSender<ChainResponse>,
     #[behaviour(ignore)]
@@ -60,13 +59,41 @@ impl StateBehavior {
         let mut behavior = Self {
             state,
             floodsub: Floodsub::new(*PEER_ID),
-            mdns: Mdns::new(Default::default()).await.expect(""),
             response_sender,
             init_sender,
         };
         behavior.floodsub.subscribe(CHAIN_TOPIC.clone());
         behavior.floodsub.subscribe(BLOCK_TOPIC.clone());
         behavior
+    }
+}
+
+impl From<FloodsubEvent> for StateBehavior {
+    fn from(event: FloodsubEvent) {
+        if let FloodsubEvent::Message(msg) = event {
+            if let Ok(resp) = serde_json::from_slice::<ChainResponse>(&msg.data) {
+                if resp.receiver == PEER_ID.to_string() {
+                    info!("Response from {}:", msg.source);
+                    resp.blocks.iter().for_each(|r| info!("{:?}", r));
+
+                    self.state.blocks = State::choose_chain(self.state.blocks.clone(), resp.blocks);
+                }
+            } else if let Ok(resp) = serde_json::from_slice::<LocalChainRequest>(&msg.data) {
+                info!("sending local chain to {}", msg.source.to_string());
+                let peer_id = resp.from_peer_id;
+                if PEER_ID.to_string() == peer_id {
+                    if let Err(e) = self.response_sender.send(ChainResponse {
+                        blocks: self.state.blocks.clone(),
+                        receiver: msg.source.to_string(),
+                    }) {
+                        error!("error sending response via channel, {}", e);
+                    }
+                }
+            } else if let Ok(block) = serde_json::from_slice(&msg.data) {
+                info!("received new block from {}", msg.source.to_string());
+                self.state.add_block(block);
+            }
+        }
     }
 }
 
