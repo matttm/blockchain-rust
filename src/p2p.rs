302224681832b925847b947c7308db4b49f69e8f
matttm <matttmaloney@gmail.com>
2024-04-28 16:12:19 -0400

    just read that struct fielda cant be ignored when impl NetworkBehaviour fuckgit add .

--- src/p2p.rs
+++ src/p2p.rs
@@ -50,90 +50,94 @@ impl From<ChainResponse> for EventType {
         Self::LocalChainResponse(event)
     }
 }
+impl From<String> for EventType {
+    fn from(event: String) -> Self {
+        EventType::Input(event)
+    }
+}
+impl From<()> for EventType {
+    fn from(_: ()) -> Self {
+        EventType::Init
+    }
+}
 
 #[derive(NetworkBehaviour)]
 //#[behaviour(event_process = true)]
 #[behaviour(to_swarm = "EventType")]
 pub struct StateBehavior {
     pub floodsub: Floodsub,
-    #[behaviour(ignore)]
-    pub response_sender: mpsc::UnboundedSender<ChainResponse>,
-    #[behaviour(ignore)]
-    pub init_sender: mpsc::UnboundedSender<bool>,
-    #[behaviour(ignore)]
-    pub state: State,
 }
 
-impl StateBehavior {
-    pub async fn new(
-        state: State,
-        response_sender: mpsc::UnboundedSender<ChainResponse>,
-        init_sender: mpsc::UnboundedSender<bool>,
-    ) -> Self {
-        let mut behavior = Self {
-            state,
-            floodsub: Floodsub::new(*PEER_ID),
-            response_sender,
-            init_sender,
-        };
-        behavior.floodsub.subscribe(CHAIN_TOPIC.clone());
-        behavior.floodsub.subscribe(BLOCK_TOPIC.clone());
-        behavior
-    }
-}
-
-impl NetworkBehaviour for StateBehavior {
-    type ConnectionHandler = OneShotHandler<FloodsubProtocol, FloodsubRpc, FloodsubEvent>;
-    type ToSwarm = EventType;
-    // Required methods
-    fn handle_established_inbound_connection(
-        &mut self,
-        _connection_id: ConnectionId,
-        peer: PeerId,
-        local_addr: &Multiaddr,
-        remote_addr: &Multiaddr,
-    ) -> Result<Self::ConnectionHandler, ConnectionDenied> {
-        self.floodsub.handle_established_inbound_connection(
-            _connection_id,
-            peer,
-            local_addr,
-            remote_addr,
-        )
-    }
-    fn handle_established_outbound_connection(
-        &mut self,
-        _connection_id: ConnectionId,
-        peer: PeerId,
-        addr: &Multiaddr,
-        role_override: Endpoint,
-    ) -> Result<Self::ConnectionHandler, ConnectionDenied> {
-        self.floodsub.handle_established_outbound_connection(
-            _connection_id,
-            peer,
-            addr,
-            role_override,
-        )
-    }
-    fn on_swarm_event(&mut self, event: FromSwarm<'_>) {
-        self.floodsub.on_swarm_event(event)
-    }
-    fn on_connection_handler_event(
-        &mut self,
-        _peer_id: PeerId,
-        _connection_id: ConnectionId,
-        _event: <Self::ConnectionHandler as ConnectionHandler>::ToBehaviour,
-    ) {
-        self.floodsub
-            .on_connection_handler_event(_peer_id, _connection_id, _event)
-    }
-    fn poll(
-        &mut self,
-        cx: &mut Context<'_>,
-    ) -> Poll<ToSwarm<Self::ToSwarm, <Self::ConnectionHandler as ConnectionHandler>::FromBehaviour>>
-    {
-        self.floodsub.poll(cx)
-    }
-}
+// impl StateBehavior {
+//     pub async fn new(
+//         state: State,
+//         response_sender: mpsc::UnboundedSender<ChainResponse>,
+//         init_sender: mpsc::UnboundedSender<bool>,
+//     ) -> Self {
+//         let mut behavior = Self {
+//             state,
+//             floodsub: Floodsub::new(*PEER_ID),
+//             response_sender,
+//             init_sender,
+//         };
+//         behavior.floodsub.subscribe(CHAIN_TOPIC.clone());
+//         behavior.floodsub.subscribe(BLOCK_TOPIC.clone());
+//         behavior
+//     }
+// }
+//
+// impl NetworkBehaviour for StateBehavior {
+//     type ConnectionHandler = OneShotHandler<FloodsubProtocol, FloodsubRpc, FloodsubEvent>;
+//     type ToSwarm = EventType;
+//     // Required methods
+//     fn handle_established_inbound_connection(
+//         &mut self,
+//         _connection_id: ConnectionId,
+//         peer: PeerId,
+//         local_addr: &Multiaddr,
+//         remote_addr: &Multiaddr,
+//     ) -> Result<Self::ConnectionHandler, ConnectionDenied> {
+//         self.floodsub.handle_established_inbound_connection(
+//             _connection_id,
+//             peer,
+//             local_addr,
+//             remote_addr,
+//         )
+//     }
+//     fn handle_established_outbound_connection(
+//         &mut self,
+//         _connection_id: ConnectionId,
+//         peer: PeerId,
+//         addr: &Multiaddr,
+//         role_override: Endpoint,
+//     ) -> Result<Self::ConnectionHandler, ConnectionDenied> {
+//         self.floodsub.handle_established_outbound_connection(
+//             _connection_id,
+//             peer,
+//             addr,
+//             role_override,
+//         )
+//     }
+//     fn on_swarm_event(&mut self, event: FromSwarm<'_>) {
+//         self.floodsub.on_swarm_event(event)
+//     }
+//     fn on_connection_handler_event(
+//         &mut self,
+//         _peer_id: PeerId,
+//         _connection_id: ConnectionId,
+//         _event: <Self::ConnectionHandler as ConnectionHandler>::ToBehaviour,
+//     ) {
+//         self.floodsub
+//             .on_connection_handler_event(_peer_id, _connection_id, _event)
+//     }
+//     fn poll(
+//         &mut self,
+//         cx: &mut Context<'_>,
+//     ) -> Poll<ToSwarm<Self::ToSwarm, <Self::ConnectionHandler as ConnectionHandler>::FromBehaviour>>
+//     {
+//         self.floodsub.poll(cx)
+//     }
+// }
 
 pub fn get_peer_list(swarm: &Swarm<StateBehavior>) -> Vec<String> {
     info!("Getting peer list...");
