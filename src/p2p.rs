06aa110882ae131aab46dbe31521138939a34cbb
matttm <matttmaloney@gmail.com>
2024-04-28 20:51:08 -0400

    implementing from for ventType

--- src/p2p.rs
+++ src/p2p.rs
@@ -5,7 +5,7 @@ use libp2p::{
     core::{Endpoint, Multiaddr},
     floodsub::{
         protocol::{FloodsubProtocol, FloodsubRpc},
-        Floodsub, FloodsubEvent, Topic,
+        Floodsub, FloodsubEvent, FloodsubMessage, Topic,
     },
     identity,
     swarm::{
@@ -60,6 +60,14 @@ impl From<()> for EventType {
         EventType::Init
     }
 }
+impl From<FloodsubEvent> for EventType {
+    fn from(event: FloodsubEvent) -> Self {
+        match event {
+            FloodsubEvent::Message(message) => {}
+            _ => println!("Someone subscribed or unsubscribed"),
+        }
+    }
+}
 
 #[derive(NetworkBehaviour)]
 //#[behaviour(event_process = true)]
@@ -68,76 +76,17 @@ pub struct StateBehavior {
     pub floodsub: Floodsub,
 }
 
-// impl StateBehavior {
-//     pub async fn new(
-//         state: State,
-//         response_sender: mpsc::UnboundedSender<ChainResponse>,
-//         init_sender: mpsc::UnboundedSender<bool>,
-//     ) -> Self {
-//         let mut behavior = Self {
-//             state,
-//             floodsub: Floodsub::new(*PEER_ID),
-//             response_sender,
-//             init_sender,
-//         };
-//         behavior.floodsub.subscribe(CHAIN_TOPIC.clone());
-//         behavior.floodsub.subscribe(BLOCK_TOPIC.clone());
-//         behavior
-//     }
-// }
+impl StateBehavior {
+    pub async fn new() -> Self {
+        let mut behavior = Self {
+            floodsub: Floodsub::new(*PEER_ID),
+        };
+        behavior.floodsub.subscribe(CHAIN_TOPIC.clone());
+        behavior.floodsub.subscribe(BLOCK_TOPIC.clone());
+        behavior
+    }
+}
 //
-// impl NetworkBehaviour for StateBehavior {
-//     type ConnectionHandler = OneShotHandler<FloodsubProtocol, FloodsubRpc, FloodsubEvent>;
-//     type ToSwarm = EventType;
-//     // Required methods
-//     fn handle_established_inbound_connection(
-//         &mut self,
-//         _connection_id: ConnectionId,
-//         peer: PeerId,
-//         local_addr: &Multiaddr,
-//         remote_addr: &Multiaddr,
-//     ) -> Result<Self::ConnectionHandler, ConnectionDenied> {
-//         self.floodsub.handle_established_inbound_connection(
-//             _connection_id,
-//             peer,
-//             local_addr,
-//             remote_addr,
-//         )
-//     }
-//     fn handle_established_outbound_connection(
-//         &mut self,
-//         _connection_id: ConnectionId,
-//         peer: PeerId,
-//         addr: &Multiaddr,
-//         role_override: Endpoint,
-//     ) -> Result<Self::ConnectionHandler, ConnectionDenied> {
-//         self.floodsub.handle_established_outbound_connection(
-//             _connection_id,
-//             peer,
-//             addr,
-//             role_override,
-//         )
-//     }
-//     fn on_swarm_event(&mut self, event: FromSwarm<'_>) {
-//         self.floodsub.on_swarm_event(event)
-//     }
-//     fn on_connection_handler_event(
-//         &mut self,
-//         _peer_id: PeerId,
-//         _connection_id: ConnectionId,
-//         _event: <Self::ConnectionHandler as ConnectionHandler>::ToBehaviour,
-//     ) {
-//         self.floodsub
-//             .on_connection_handler_event(_peer_id, _connection_id, _event)
-//     }
-//     fn poll(
-//         &mut self,
-//         cx: &mut Context<'_>,
-//     ) -> Poll<ToSwarm<Self::ToSwarm, <Self::ConnectionHandler as ConnectionHandler>::FromBehaviour>>
-//     {
-//         self.floodsub.poll(cx)
-//     }
-// }
 
 pub fn get_peer_list(swarm: &Swarm<StateBehavior>) -> Vec<String> {
     info!("Getting peer list...");
